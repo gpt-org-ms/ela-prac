6293aeec23737fd2ab65354de9d6a105b6f17f54
ms1011 <wkdalstjr94@gmail.com>
2024-05-08 11:22:24 +0900

    이상한 코드 push

--- src/main/java/com/ohgiraffers/elaprac/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/elaprac/controller/MenuController.java
@@ -15,7 +15,7 @@ public class MenuController {
 
     @GetMapping("health")
     public String healthCheck() {
-        return "fine!";
+        return "fine!@#@$@#";
     }
 
     @GetMapping("menus/{menuCode}")
